28f6eb0dd55eaf6ed0fdc2537d89a3b62fe7e537
mois3222 <moisky222@gmail.com>
2023-06-21 14:13:22 -0500

    ready for do main

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import "./styles/main.scss";
 import Routes from "./routes/Routes";
+import { gameService } from "./utils/getGame";
 
 const routes = Routes.getInstance();
 
 window.addEventListener("load", () => {
   routes.renders();
+  console.log(gameService.getGame);
 });
 console.log("working..");
--- src/utils/getGame.ts
+++ src/utils/getGame.ts
@@ -1,17 +1,64 @@
 import GameService from "../services/game.service";
 
-const gameService = GameService.getInstance();
+export const gameService = GameService.getInstance();
 
-gameService.getGame();
+gameService.addGame({
+  name: "Zrist",
+  image: "../assets/zrist-back.jpg",
+  url: "https://html5.gamemonetize.com/iyq1x3g396ik7vhf71czo9ubmjrimzd4/",
+  completed: false,
+});
 gameService.addGame({
   name: "Mini Switcher",
-  image: "imgs/miniswitcher-back.jpg",
+  image: "../assets/miniswitcher-back.jpg",
   url: "https://html5.gamemonetize.com/r822rwum14259b7wacvt1acbp3e4jv4a/",
-  completed: true,
+  completed: false,
 });
-
-gameService.update(9, {
-  name: "hsjhdasd",
+gameService.addGame({
+  name: "FireBlob",
+  image: "../assets/fireblob-back.jpg",
+  url: "https://html5.gamemonetize.com/18maefikrn6r5isrep10icvc6lzfrfft/",
+  completed: false,
 });
-
-gameService.filterGames(gameService.arrGames, "id", 12);
+gameService.addGame({
+  name: "Mini Blocks",
+  image: "../assets/miniblocks-back.jpg",
+  url: "https://html5.gamemonetize.com/a3g08anaetn716zmnmike7tvgokee4ip/",
+  completed: false,
+});
+gameService.addGame({
+  name: "Mini Colors",
+  image: "../assets/minicolors-back.jpg",
+  url: "https://html5.gamemonetize.com/qdi48bsj1weirf128p32hoagcw2q9kr0/",
+  completed: false,
+});
+gameService.addGame({
+  name: "Emerald And Amber",
+  image: "../assets/emeraldandamber-back.jpg",
+  url: "https://html5.gamemonetize.com/qjyxz4nan9jjcwed12rspodfjqrsc773/",
+  completed: false,
+});
+gameService.addGame({
+  name: "Mini Push",
+  image: "../assets/minipush-back.jpg",
+  url: "https://html5.gamemonetize.com/fkcezxby2ig5nff714fgjvc287a7cd7s/",
+  completed: false,
+});
+gameService.addGame({
+  name: "FireBlob Winter",
+  image: "../assets/fireblobwinter-back.jpg",
+  url: "https://html5.gamemonetize.com/pj05r8tubjj0ukl65o5t9ha5pp111a2r/",
+  completed: false,
+});
+gameService.addGame({
+  name: "Gum Adventures DX",
+  image: "../assets/gumdx-back.jpg",
+  url: "https://html5.gamemonetize.com/ej2w8vnngsep8kcqsfxae8iyg6ghuci9/",
+  completed: false,
+});
+gameService.addGame({
+  name: "Mini Arrows",
+  image: "../assets/miniarrows-back.jpg",
+  url: "https://html5.gamemonetize.com/f1eta6xj4p282uxtt4fomstuq9sbkwcz/",
+  completed: false,
+});
